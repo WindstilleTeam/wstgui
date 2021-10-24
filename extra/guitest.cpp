9118d4771e5e4249f5bf354952349988070e1ebc
Ingo Ruhnke <grumbel@gmail.com>
2021-10-24 18:17:14 +0200

    Add more widgets to extra/guitest

--- extra/guitest.cpp
+++ extra/guitest.cpp
@@ -19,17 +19,20 @@
 #include <SDL.h>
 
 #include <surf/palette.hpp>
-#include <wstdisplay/opengl_window.hpp>
 #include <wstdisplay/font/ttf_font_manager.hpp>
+#include <wstdisplay/opengl_window.hpp>
+#include <wstgui/button.hpp>
 #include <wstgui/gui_manager.hpp>
-#include <wstgui/root_component.hpp>
 #include <wstgui/label.hpp>
+#include <wstgui/menu.hpp>
+#include <wstgui/root_component.hpp>
 #include <wstgui/style.hpp>
+#include <wstgui/text_view.hpp>
 #include <wstinput/controller.hpp>
 
 using namespace wstinput;
 using namespace wstdisplay;
-using namespace gui;
+using namespace wstgui;
 
 int main()
 {
@@ -44,12 +47,67 @@ int main()
 
   TTFFontManager ttf_font_manager;
   std::unique_ptr<TTFFont> font = ttf_font_manager.create_font("external/wstdisplay/extra/Vera.ttf", 20);
+  std::unique_ptr<TTFFont> smallfont = ttf_font_manager.create_font("external/wstdisplay/extra/Vera.ttf", 16);
   Style style(font.get());
   GUIManager gui_manager(style);
   auto* root = gui_manager.get_root();
-  auto label = std::make_unique<Label>("Hello World", root);
-  label->set_screen_rect(geom::frect(100, 100, 500, 300));
+
+  auto label = std::make_unique<Label>("GUI Test", root);
+  label->set_screen_rect(geom::frect(20, 20, 100, 50));
   root->add_child(std::move(label));
+
+  auto textview = std::make_unique<TextView>(geom::frect(20, 50, 620, 400), root);
+  textview->set_font(smallfont.get());
+  textview->set_text(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in "
+    "consectetur sapien. Pellentesque a cursus est. Vestibulum nec "
+    "ultricies sapien, et tristique dui. Donec pulvinar turpis sed risus "
+    "eleifend, eget malesuada tortor lacinia. Aliquam ut dolor purus. "
+    "Mauris porta diam a iaculis aliquet. Phasellus maximus arcu sed nisi "
+    "efficitur viverra. Donec sed libero sed dui varius sollicitudin eu non "
+    "elit. Quisque eu arcu ornare, feugiat urna non, consectetur odio. Duis "
+    "lobortis et leo at finibus. Aliquam condimentum ligula id nulla "
+    "facilisis mollis.\n"
+    "\n"
+    "Duis vulputate consequat ipsum in suscipit. Nunc porttitor convallis "
+    "eros, sit amet gravida justo lobortis nec. Vivamus vehicula velit eu "
+    "nisl fringilla, at posuere sem placerat. Class aptent taciti sociosqu "
+    "ad litora torquent per conubia nostra, per inceptos himenaeos. Proin "
+    "vel magna risus. Integer ullamcorper, dui eu fringilla sollicitudin, "
+    "nisi lorem varius massa, sit amet vehicula sem lorem nec nulla. Nam "
+    "volutpat condimentum mi in vestibulum. Maecenas malesuada risus non "
+    "lectus egestas blandit. Nullam convallis nunc quis tellus vulputate "
+    "congue. Cras id diam vitae nisi rhoncus congue. Sed nisi neque, ornare "
+    "a leo et, vehicula condimentum sapien. Maecenas maximus consequat "
+    "tellus vel condimentum. Maecenas imperdiet elit quis enim auctor "
+    "lacinia.\n"
+    "\n"
+    "Phasellus vel suscipit massa. Phasellus in magna nec magna placerat "
+    "egestas. Nullam id condimentum lacus, ac bibendum est. Donec sit amet "
+    "facilisis tellus. Ut molestie non tortor sit amet porta. Duis feugiat "
+    "scelerisque ipsum non hendrerit. Aenean vel odio faucibus, pharetra "
+    "felis id, pharetra felis. Donec tempus augue lorem, at suscipit tortor "
+    "aliquet et. Phasellus molestie justo in odio dignissim, sit amet "
+    "molestie ex convallis. Suspendisse vitae tortor sapien.\n"
+    );
+  root->add_child(std::move(textview));
+
+  auto ok_button = std::make_unique<Button>("Ok", root);
+  auto cancel_button = std::make_unique<Button>("Cancel", root);
+  ok_button->set_screen_rect(geom::frect(500, 420, 600, 460));
+  cancel_button->set_screen_rect(geom::frect(380, 420, 480, 460));
+  root->add_child(std::move(ok_button));
+  root->add_child(std::move(cancel_button));
+
+  auto menu = std::make_unique<wstgui::Menu>("Options", geom::frect(geom::fpoint(100, 100), geom::fsize(500, 340)), style);
+  menu->add_slider("Master Volume",  50, 0, 100, 10, {});
+  menu->add_slider("Music Volume", 75, 100, 10, {});
+  menu->add_slider("Sound Volume", 100, 0, 100, 10, {});
+  menu->add_slider("Voice Volume", 50, 0, 100, 10, {});
+  //root->add_child(std::move(menu));
+
+  //ScreenManager screen_manager;
+  //screen_manager.run();
 
   bool quit = false;
   while (!quit) {
@@ -79,6 +137,7 @@ int main()
 
     gc.clear(surf::palette::black);
     gui_manager.draw(gc);
+    menu->get_root()->draw(gc);
     window.swap_buffers();
 
     gui_manager.update(1.0f / 60, Controller());
